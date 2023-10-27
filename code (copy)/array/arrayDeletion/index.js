5ded41543c728e51ba6583b1d74d090d23c096d9
Arshil Hapani <arshilhapani998@gmail.com>
2023-10-27 11:18:16 +0530

    Exam commit

--- code (copy)/array/arrayDeletion/index.js
+++
@@ -1,8 +0,0 @@
-function insertAtIndex(arr, size, index) {
-  for (let i = index; i < size; i++) {
-    arr[i] = arr[i + 1];
-  }
-}
-let arr = [10, 2, 4, 5, null, null];
-insertAtIndex(arr, 4, 2);
-console.log({ arr });
