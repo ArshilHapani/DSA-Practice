6f25602e557d1980a631516d9b0d23f678049cf9
ArshilHapani <arshilhapani998@gmail.com>
2023-12-08 21:23:36 +0530

    Rotation done

--- udemy_course/sec2/RotateArray.cpp
+++ udemy_course/sec2/RotateArray.cpp
@@ -3,20 +3,37 @@
 #include <algorithm>
 
 using namespace std;
-
 void rotateArray(vector<int> &arr, int k)
 {
-    reverse(arr.begin(), arr.end());
+    int n = arr.size();
+    k %= n;
 
-    reverse(arr.begin(), arr.end() + k);
+    vector<int> tempArr(n);
 
-    reverse(arr.begin() + k, arr.end());
+    for (int i = 0; i < n; ++i)
+    {
+        tempArr[(i + k) % n] = arr[i];
+    }
+    arr = tempArr;
+}
+
+void reverseL(vector<int> &arr, int start, int end)
+{
+    reverse(arr.begin() + start, arr.begin() + end + 1);
+}
+
+void smartRotate(vector<int> &arr, int k)
+{
+    k %= arr.size();
+    reverseL(arr, 0, arr.size() - 1);
+    reverseL(arr, 0, k - 1);
+    reverseL(arr, k, arr.size() - 1);
 }
 
 int main()
 {
     vector<int> arr = {1, 2, 3, 4};
-    int k = 2;
+    int k = 3;
     rotateArray(arr, k);
 
     for (int item : arr)
