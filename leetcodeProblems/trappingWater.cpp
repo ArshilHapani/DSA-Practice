775c4624e4d95d8b540e6685a73cc2f91da1f760
ArshilHapani <arshilhapani998@gmail.com>
2023-12-10 22:35:08 +0530

    Comtainer problem done

---
+++ leetcodeProblems/trappingWater.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int trap(vector<int> &height)
+    {
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution s;
+
+    return 0;
+}
